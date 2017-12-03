a3c3fb4b9676d04e10f617372e730379e9bd7f83
Guilherme Piaia <piaiag2@gmail.com>
2017-12-03 18:00:33 -0200

    PID funcionando, removido o filtro, step = 100

--- MScript/AutoPID.m
+++ MScript/AutoPID.m
@@ -2,9 +2,15 @@
 clc
 clear
 
+%state = 'Org';
 state = 'PID';
+%state = 'PI';
+
 TypeControler = 1; % 1-Sem controle; 2-PID; 2-PID Anti-Windup
 TypeOutput = 1; %  1-Sistema; 2-Degrau usado para simular; 3-Atraso de transporte 
+T_PID = 1; % 1-PID; 2-PI+D
+N = 1;
+Malha = 1;
 
 K=1;
 Kp = 1;
@@ -15,23 +21,18 @@ tau = 1;
 theta = 1;
 
 switch state
-    
-    case 'PID'
+    case 'Org'
         data = SSimulink(1,1);
-        %CTauTheta(data.Time(), FilterData(data));
-        %plot(SSimulink(1,3));
-        CalcPID(tau, theta);
-    case 'pid'
+        plot(data)
+    case 'PID'
+        Malha = 2; %Abre a malha
+        data = SSimulink(1,1);
+        CTauTheta(data);
+        CalcPID('PID',tau, theta);
+        Malha = 1; %Fecha a Malha
+        plot(SSimulink(2,1))
     case 'tunnunig'
     case 'show'       
-end
-
-% Filtro do sinal para possibilitar os cálculos de tau e theta
-function [avg] = FilterData(data)
-        wSize = 15; 
-        b = (1/wSize)*ones(1,wSize);
-        a = 1;
-        avg = filter(b, 1, data().Data());
 end
 
 % Função para simular no simulink e visualizar as saídas
@@ -45,20 +46,20 @@ function [outputs] = SSimulink(a, b)
         outputs = simOut.get('simout');
 end
 
-function [] = CTauTheta(time, data)
+function [] = CTauTheta(data)
 
-        len = length(time);
-        K = data(len); % Valor em regime  permanente
+        len = length(data.Time());
+        K = data.Data(len); % Valor em regime  permanente
         assignin('base', 'K', K);
         % Laço para buscar os valores de tempo da resposta transitória
         c_t1 = 0;
         for i=1:len
-            if(data(i)>=K*0.283 && c_t1==0)
-                t1 = time(i);
+            if(data.Data(i)>=K*0.283 && c_t1==0)
+                t1 = data.Time(i);
                 c_t1 = 1;
             end
-            if(data(i)>=K*0.632)
-                t2 = time(i);
+            if(data.Data(i)>=K*0.632)
+                t2 = data.Time(i);
                 break
             end
         end
@@ -66,16 +67,16 @@ function [] = CTauTheta(time, data)
         A = [1 1/3; 1 1];
         B = [t1 ; t2];
         C= linsolve(A,B);
-        tau = C(1,1);
-        theta = C(2,1);
+        theta = C(1,1);
+        tau = C(2,1);
         assignin('base', 'tau', tau);
         assignin('base', 'theta', theta);
 end
 
 % Função para o cálculo dos parâmetros dos diferentes controladores
 function CalcPID(type_controler, tau, theta)
-    rtt = theta/tau; % Relação entre tau e theta
     
+    rtt = theta/tau;
     switch type_controler
         case 'P'
             if rtt> 0.1 && rtt < 0.4
@@ -101,15 +102,15 @@ function CalcPID(type_controler, tau, theta)
                 Td = 0;
             end            
         case 'PID'
-            if rtt> 0.1 && rtt < 0.4
-                Kp = 0.9*rtt^-1;
-                Ti = 3.33*tau*rtt;
-                Td = 0;
+            if rtt> 0.1 && rtt <= 0.4
+                Kp = 1.2*tau/theta;
+                Ti = 2*theta;
+                Td = 0.5*theta;
             end
-            if rtt >0.6 && rtt < 4.5
-                Kp = 0.9*rtt^-1 + 0.082;
-                Ti = 3.33*tau*rtt*(1+rtt/11)/(1+2.2*rtt);
-                Td = 0;
+            if rtt >0.4 && rtt < 4.5
+                Kp = 1.35*rtt^-1 + 0.27;
+                Ti = 2.5*theta*(1+rtt/5)/(1+0.6*rtt);
+                Td = 0.37*theta/(1+0.2*rtt);
             end  
     end
     assignin('base', 'Kp', Kp);
